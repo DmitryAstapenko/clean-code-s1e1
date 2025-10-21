16ae2f50bae6303fa6683c944bbd1da6e3bf0ea8
DmitryAstapenko <dmitryastapenko1@gmail.com>
2025-10-21 12:20:49 +0300

    fix(extended-1.2): add alt attribute to remove image in JS

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="todo__btn-remove btn";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "todo__btn-remove-img";
+    deleteButtonImg.alt = "remove";    
     deleteButton.appendChild(deleteButtonImg);
 
 
